60fc5cd448a5515c332d2eb2785af592eba0561b
olenapyrig <olenka.pyrig@gmail.com>
2019-12-18 20:49:49 +0200

    #3 Created Asset Model and UI

--- airport/airport-pojo-bl/src/main/java/singapore/config/ApplicationDomain.java
+++ airport/airport-pojo-bl/src/main/java/singapore/config/ApplicationDomain.java
@@ -12,6 +12,7 @@ import ua.com.fielden.platform.domain.PlatformDomainTypes;
 import ua.com.fielden.platform.entity.AbstractEntity;
 import singapore.asset.tablecodes.AssetClass;
 import singapore.asset.tablecodes.AssetType;
+import singapore.assets.Asset;
 
 /**
  * A class to register domain entities.
@@ -28,6 +29,7 @@ public class ApplicationDomain implements IApplicationDomainProvider {
         add(Person.class);
         add(AssetClass.class);
         add(AssetType.class);
+        add(Asset.class);
     }
 
     private static void add(final Class<? extends AbstractEntity<?>> domainType) {
--- airport/airport-web-server/src/main/java/singapore/webapp/WebUiConfig.java
+++ airport/airport-web-server/src/main/java/singapore/webapp/WebUiConfig.java
@@ -4,9 +4,11 @@ import org.apache.commons.lang.StringUtils;
 
 import singapore.asset.tablecodes.AssetClass;
 import singapore.asset.tablecodes.AssetType;
+import singapore.assets.Asset;
 import singapore.config.personnel.PersonWebUiConfig;
 import singapore.webapp.config.asset.tablecodes.AssetClassWebUiConfig;
 import singapore.webapp.config.asset.tablecodes.AssetTypeWebUiConfig;
+import singapore.webapp.config.assets.AssetWebUiConfig;
 import ua.com.fielden.platform.basic.config.Workflows;
 import ua.com.fielden.platform.web.app.config.IWebUiBuilder;
 import ua.com.fielden.platform.web.interfaces.ILayout.Device;
@@ -75,6 +77,7 @@ public class WebUiConfig extends AbstractWebUiConfig {
         //UI for table codes for assets
         final AssetClassWebUiConfig assetClassWebUiConfig = AssetClassWebUiConfig.register(injector(), builder);
         final AssetTypeWebUiConfig assetTypeWebUiConfig = AssetTypeWebUiConfig.register(injector(), builder);
+        final AssetWebUiConfig assetWebUiConfig = AssetWebUiConfig.register(injector(), builder);
         
         // Configure application web resources such as masters and centres
         configApp()
@@ -88,30 +91,45 @@ public class WebUiConfig extends AbstractWebUiConfig {
 
         // Configure application menu
         configDesktopMainMenu().
+        addModule("Asset Acquisition").
+            description("Asset acquisition module").
+            icon("mainMenu:equipment").
+            detailIcon("mainMenu:equipment").
+            bgColor("#FFE680").
+            captionBgColor("#FFD42A").menu()
+                .addMenuItem(Asset.ENTITY_TITLE).description(String.format("%s Centre", Asset.ENTITY_TITLE))
+                .centre(assetWebUiConfig.centre).done()
+                .done().done().
             addModule("Users / Personnel").
                 description("Provides functionality for managing application security and personnel data.").
                 icon("mainMenu:help").
-                detailIcon("mainMenu:help").
+                detailIcon("anotherMainMenu:about").
                 bgColor("#FFE680").
                 captionBgColor("#FFD42A").menu()
-                .addMenuItem("Asset Table Codes").description("Various master data for assets.")
-                    .addMenuItem(AssetClass.ENTITY_TITLE).description(String.format("%s Centre", AssetClass.ENTITY_TITLE))
-                    .centre(assetClassWebUiConfig.centre).done()
-                    .addMenuItem(AssetType.ENTITY_TITLE).description(String.format("%s Centre", AssetType.ENTITY_TITLE))
-                    .centre(assetTypeWebUiConfig.centre).done()
-                 .done()
                 .addMenuItem("Personnel").description("Personnel related data")
                     .addMenuItem("Personnel").description("Personnel Centre").centre(personWebUiConfig.centre).done()
                 .done()
                 .addMenuItem("Users").description("Users related data")
                     .addMenuItem("Users").description("User centre").centre(userWebUiConfig.centre).done()
                     .addMenuItem("User Roles").description("User roles centre").centre(userRoleWebUiConfig.centre).done()
-                .done()
-            .done().done()
-        .setLayoutFor(Device.DESKTOP, null, "[[[]]]")
-        .setLayoutFor(Device.TABLET, null, "[[[]]]")
-        .setLayoutFor(Device.MOBILE, null, "[[[]]]")
-        .minCellWidth(100).minCellHeight(148).done();
-    }
-
+                .done().
+            done().done().
+            addModule("Table Codes").
+            description("Table Codes Description").
+            icon("mainMenu:tablecodes").
+            detailIcon("mainMenu:tablecodes").
+            bgColor("#FFE680").
+            captionBgColor("#FFD42A").menu()
+            .addMenuItem("Asset Table Codes").description("Various master data for assets.")
+                .addMenuItem(AssetClass.ENTITY_TITLE).description(String.format("%s Centre", AssetClass.ENTITY_TITLE))
+                .centre(assetClassWebUiConfig.centre).done()
+                .addMenuItem(AssetType.ENTITY_TITLE).description(String.format("%s Centre", AssetType.ENTITY_TITLE))
+                .centre(assetTypeWebUiConfig.centre).done()
+            .done().
+        done().done()
+    .setLayoutFor(Device.DESKTOP, null, "[[[{\"rowspan\":2}], []], [[]]]")
+    .setLayoutFor(Device.TABLET, null,  "[[[{\"rowspan\":2}], []], [[]]]")
+    .setLayoutFor(Device.MOBILE, null, "[[[]],[[]], [[]]]")
+    .minCellWidth(100).minCellHeight(148).done();
+}
 }
